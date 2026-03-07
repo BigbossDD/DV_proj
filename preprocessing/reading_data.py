fa7213fa9a99fa5d9ea730d28fab53c8a8328701
BigbossDD <Legojaddai@gmail.com>
2026-03-07 17:51:57 +0300

    Refactor null handling in data preprocessing and improve comments

--- preprocessing/reading_data.py
+++ preprocessing/reading_data.py
@@ -11,8 +11,20 @@ print(data.store_and_fwd_flag.head(3))
 for column in data.columns:
     
     print(column , ' : ' , data[column].dtype)
-# after checking , their are data type , and string type  
+# after checking , their are data type , and string type we will keep that 
+#in mind for now 
+
+
+
 
 
 #checking for nulls 
 
+print(data.isnull().sum())
+# nulls do exisit in and how we will dealwith it  {passenger_count:mean/mode ,RatecodeID: ?,  store_and_fwd_flag:mode ,payment_type : ??? , 
+# congestion_surcharge : mode ,Airport_fee : mean}
+
+data['passenger_count'].fillna(data['passenger_count'].mean(), inplace=True)
+data['store_and_fwd_flag'].fillna(data['store_and_fwd_flag'].mode()[0], inplace=True)
+data['congestion_surcharge'].fillna(data['congestion_surcharge'].mode()[0], inplace=True)
+data['Airport_fee'].fillna(data['Airport_fee'].mean(), inplace=True)        
